1df2bf3f4b8330579faef4526761bf694c333508
Khaled Saad <khaledsoultan63@gmail.com>
2020-12-26 22:11:51 +0000

    Fixed bugs in map utils

--- include/monte_carlo_localisation/map_utils.hpp
+++ include/monte_carlo_localisation/map_utils.hpp
@@ -1,16 +1,18 @@
 #ifndef B8A400C8_F58A_4931_8E05_BBB95F2BF8F6
 #define B8A400C8_F58A_4931_8E05_BBB95F2BF8F6
 
+#include "nav_msgs/OccupancyGrid.h"
 #include <cstdint>
 #include <utility>
 
 class MapUtils
 {
 public:
-    static int coordinates_to_index(const uint32_t &x, const uint32_t &y, const uint32_t &map_width);
-    static std::pair<uint32_t, uint32_t> index_to_coordinates(const int &index, const uint32_t &map_width);
-    static double distance_between_two_points(const uint32_t &x1, const uint32_t &y1,
-                                              const uint32_t &x2, const uint32_t &y2);
+    static int map_coordinates_to_index(const double &x, const double &y, const uint32_t &map_width);
+    static std::pair<int, int> world_coord_to_map_coord(const double &x, const double &y, const nav_msgs::MapMetaData &map_info);
+    static std::pair<uint32_t, uint32_t> map_index_to_coordinates(const int &index, const uint32_t &map_width);
+    static double distance_between_two_points(const int &x1, const int &y1,
+                                              const int &x2, const int &y2);
 };
 
 #endif /* B8A400C8_F58A_4931_8E05_BBB95F2BF8F6 */
--- src/monte_carlo_localisation/map_utils.cpp
+++ src/monte_carlo_localisation/map_utils.cpp
@@ -1,18 +1,24 @@
 #include "monte_carlo_localisation/map_utils.hpp"
 #include <cmath>
 
-int MapUtils::coordinates_to_index(const uint32_t &x, const uint32_t &y, const uint32_t &map_width)
+int MapUtils::map_coordinates_to_index(const double &x, const double &y, const uint32_t &map_width)
 {
-    return x + y*map_width;
+    return floor(x + y*map_width);
 }
-std::pair<uint32_t, uint32_t> MapUtils::index_to_coordinates(const int &index, const uint32_t &map_width)
+std::pair<int, int> MapUtils::world_coord_to_map_coord(const double &x, const double &y, const nav_msgs::MapMetaData &map_info)
+{
+    int map_x = floor((x - map_info.origin.position.x) / map_info.resolution + 0.5);
+    int map_y = floor((y - map_info.origin.position.y) / map_info.resolution + 0.5);
+    return std::make_pair(map_x, map_y);
+}
+std::pair<uint32_t, uint32_t> MapUtils::map_index_to_coordinates(const int &index, const uint32_t &map_width)
 {
     uint32_t x = index % map_width;
     uint32_t y = (index - x)/map_width;
     return std::make_pair(x, y);
 }
-double MapUtils::distance_between_two_points(const uint32_t &x1, const uint32_t &y1,
-                                             const uint32_t &x2, const uint32_t &y2)
+double MapUtils::distance_between_two_points(const int &x1, const int &y1,
+                                             const int &x2, const int &y2)
 {
     return hypot(x1-x2, y1-y2);
 }
